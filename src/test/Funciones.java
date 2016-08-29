8797e75e27e943e5dc43b355a90ea22253928aa6
agus0 <agustinpadron94@gmail.com>
2016-08-29 10:03:15 -0300

    Se hizo el TP hasta sonFechasHorasIguales

---
+++ src/test/Funciones.java
@@ -0,0 +1,203 @@
+package test;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Funciones {
+	public static String convertirString (int numero) {
+		return String.valueOf(numero);
+	}
+	
+	public static double convertirDoble (int numero) {	
+		return Double.parseDouble(convertirString(numero));
+	}
+	
+	public static void imprimirArreglo(int[] array) {
+		int i;
+		for (i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
+	public static void imprimirArreglo(double[][] array) {
+		int i, j;
+		
+		for (i = 0; i < array.length; i++) {
+			for (j = 0; j < array[0].length; j++) {
+				System.out.print("["+array[i][j]+"]");
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public static boolean esBisiesto (int anio) {
+		boolean resultado;
+		
+		if (((anio % 4 == 0) && (anio % 100 != 0)) || (anio % 400 == 0)) {
+			resultado = true;
+		}else{
+			resultado = false;
+		}
+		return resultado;
+	}
+	
+	public static int traerAnio (GregorianCalendar fecha) {
+		return fecha.get(Calendar.YEAR);
+	}
+	
+	public static int traerMes (GregorianCalendar fecha) {
+		return fecha.get(Calendar.MONTH) + 1;
+	}
+	
+	public static int traerDia (GregorianCalendar fecha) {
+		return fecha.get(Calendar.DAY_OF_MONTH);
+	}
+	
+	public static int traerCantDiasDeUnMes (int anio, int mes) {
+		boolean bisiesto = Funciones.esBisiesto(anio);
+		int		max_dias = 31;
+		
+		if (mes >= 1 && mes <= 12) {
+			if (mes == 4 || mes == 6 || mes == 9 || mes == 11){
+				max_dias = 30;
+			}
+			if (mes == 2) {
+				if (bisiesto) {
+					max_dias = 29;
+				}else{
+					max_dias = 28;
+				}
+			}
+		}else{
+			max_dias = -1;
+		}
+		return max_dias;
+	}
+	
+	public static boolean esFechaValida(int anio, int mes, int dia) {
+		boolean resultado = true;
+		int		max_dias = traerCantDiasDeUnMes(anio, mes);
+		
+		if (dia < 1 || dia > max_dias) {
+			resultado = false;
+		}
+		return resultado;
+	}
+	
+	public static GregorianCalendar traerFecha(int anio, int mes, int dia) {
+		return new GregorianCalendar(anio, mes - 1, dia);
+	}
+	
+	//Parametro de la fecha es "DD/MM/AAAA"
+	public static GregorianCalendar traerFecha(String fecha) {
+		String[] date = new String[3];
+		
+		date = fecha.split("[/]+");
+		return traerFecha(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+	}
+	
+	public static String traerFechaCorta(GregorianCalendar fecha) {
+		return (traerDia(fecha) + "/" + traerMes(fecha) + "/" + traerAnio(fecha));
+	}
+	
+	public static String traerFechaCortaHora(GregorianCalendar fecha) {
+		return (traerDia(fecha) + "/" + traerMes(fecha) + "/" + traerAnio(fecha) + " " + fecha.get(Calendar.HOUR_OF_DAY) + ":" + fecha.get(Calendar.MINUTE) + ":" + fecha.get(Calendar.SECOND));
+	}
+	
+	public static GregorianCalendar traerFechaProximo(GregorianCalendar fecha, int dias) {
+		GregorianCalendar resultado = fecha;
+		resultado.add(Calendar.DAY_OF_MONTH, dias);
+		return resultado;
+	}
+	
+	public static boolean esDiaHabil (GregorianCalendar fecha) {
+		return (fecha.get(Calendar.DAY_OF_WEEK) > 1) && (fecha.get(Calendar.DAY_OF_WEEK) < 7);
+	}
+	
+	public static String traerDiaDeLaSemana (GregorianCalendar fecha) {
+		String resultado;
+		switch(fecha.get(Calendar.DAY_OF_WEEK)) {
+			case 1:
+				resultado = "Domingo";
+				break;
+			case 2:
+				resultado = "Lunes";
+				break;
+			case 3:
+				resultado = "Martes";
+				break;
+			case 4:
+				resultado = "Miercoles";
+				break;
+			case 5:
+				resultado = "Jueves";
+				break;
+			case 6:
+				resultado = "Viernes";
+				break;
+			case 7:
+				resultado = "Sabado";
+				break;
+			default:
+				resultado = "null";
+		}
+		return resultado;
+	}
+	
+	public static String traerMesEnLetras (GregorianCalendar fecha) {
+		String resultado;
+		
+		switch (fecha.get(Calendar.MONTH)) {
+			case 0:
+				resultado = "Enero";
+				break;
+			case 1:
+				resultado = "Febrero";
+				break;
+			case 2:
+				resultado = "Marzo";
+				break;
+			case 3:
+				resultado = "Abril";
+				break;
+			case 4:
+				resultado = "Mayo";
+				break;
+			case 5:
+				resultado = "Junio";
+				break;
+			case 6:
+				resultado = "Julio";
+				break;
+			case 7:
+				resultado = "Agosto";
+				break;
+			case 8:
+				resultado = "Septiembre";
+				break;
+			case 9:
+				resultado = "Octubre";
+				break;
+			case 10:
+				resultado = "Noviembre";
+				break;
+			case 11:
+				resultado = "Diciembre";
+				break;
+			default:
+				resultado = "null";
+		}
+		return resultado;
+	}
+	
+	public static String traerFechaLarga(GregorianCalendar fecha) {
+		return (traerDiaDeLaSemana(fecha) + " " + traerDia(fecha) + " de " + traerMesEnLetras(fecha) + " del " + traerAnio(fecha));
+	}
+	
+	public static boolean sonFechasIguales (GregorianCalendar fecha1, GregorianCalendar fecha2) {
+		return ((traerAnio(fecha1) == traerAnio(fecha2)) && (traerMes(fecha1) == traerMes(fecha2)) && (traerDia(fecha1) == traerDia(fecha2)));
+	}
+	
+	public static boolean sonFechasHorasIguales (GregorianCalendar fecha1, GregorianCalendar fecha2) {
+		return ((fecha1.get(Calendar.HOUR_OF_DAY) == fecha2.get(Calendar.HOUR_OF_DAY)) && (fecha1.get(Calendar.MINUTE) == fecha2.get(Calendar.MINUTE)));
+	}
+}
---
+++ src/test/Test.java
@@ -0,0 +1,96 @@
+package test;
+import java.util.GregorianCalendar;
+
+
+public class Test {
+
+	private static void testBisiesto() {
+		System.out.println("2005 es bisiesto?: " + Funciones.esBisiesto(2005));
+		System.out.println("2004 es bisiesto?: " + Funciones.esBisiesto(2004));
+		System.out.println("\n");
+	}
+	
+	private static void testTraerAnio(){
+		GregorianCalendar fecha = new GregorianCalendar();
+		
+		System.out.println("Año actual: " + Funciones.traerAnio(fecha));
+		System.out.println("\n");
+	}
+	
+	private static void testTraerMes(){
+		GregorianCalendar fecha = new GregorianCalendar();
+		
+		System.out.println("Mes actual: " + Funciones.traerMes(fecha));
+		System.out.println("\n");
+	}
+	
+	private static void testTraerDia(){
+		GregorianCalendar fecha = new GregorianCalendar();
+		
+		System.out.println("Dia actual: " + Funciones.traerDia(fecha));
+		System.out.println("\n");
+	}
+	
+	private static void testEsFechaValida(){
+		System.out.println("26/06/1994 es valida?: " + Funciones.esFechaValida(1994, 6, 26));
+		System.out.println("29/02/2016 es valida?: " + Funciones.esFechaValida(2016, 2, 29));
+		System.out.println("29/02/2015 es valida?: " + Funciones.esFechaValida(2015, 2, 29));
+		System.out.println("35/06/2012 es valida?: " + Funciones.esFechaValida(2012, 6, 35));
+		System.out.println("1/15/2010 es valida?: " + Funciones.esFechaValida(2010, 15, 1));
+		System.out.println("cantidad de dias de Febrero de 2016: " + Funciones.traerCantDiasDeUnMes(2016, 02));
+		System.out.println("cantidad de dias de Febrero de 2015: " + Funciones.traerCantDiasDeUnMes(2015, 02));
+		System.out.println("\n");
+	}
+	
+	private static void testTraerFecha() {
+		System.out.println("Dia en 26/06/1994: " + Funciones.traerDia(Funciones.traerFecha(1994, 06, 26)));
+		System.out.println("Mes en 15/04/2005: " + Funciones.traerMes(Funciones.traerFecha("15/04/2005")));
+		System.out.print("\n");
+	}
+	
+	private static void testTraerFechaCorta () {
+		System.out.println(Funciones.traerFechaCorta(new GregorianCalendar ()));
+		System.out.println(Funciones.traerFechaCortaHora(new GregorianCalendar ()));
+		System.out.println("En 10 dias sera: " + Funciones.traerFechaCorta(Funciones.traerFechaProximo(new GregorianCalendar(), 10)));
+		System.out.println("\n");
+	}
+	
+	private static void testEsDiaHabil () {
+		String fecha = "26/06/1994";
+		
+		System.out.println("Hoy es dia habil?: " + Funciones.esDiaHabil(new GregorianCalendar()));
+		System.out.println(fecha + " es dia habil?: " + Funciones.esDiaHabil(Funciones.traerFecha(fecha)));
+		System.out.println("\n");
+	}
+	
+	private static void testTraerDiaDeLaSemana(){
+		System.out.println("Hoy es " + Funciones.traerDiaDeLaSemana(new GregorianCalendar()) + " - " + Funciones.traerMesEnLetras(new GregorianCalendar()));
+		System.out.print("\nHoy es:\n");
+		System.out.println(Funciones.traerFechaLarga(new GregorianCalendar()));
+		System.out.println("\n");
+	}
+	
+	private static void testCompara() {
+		String fecha1 = "14/06/1954";
+		String fecha2 = "26/06/1994";
+		String f_hoy  = "23/08/2016";
+		
+		
+		System.out.println(fecha1 + " = " + fecha2 + "? " + Funciones.sonFechasIguales(Funciones.traerFecha(fecha1), Funciones.traerFecha(fecha2)));
+		System.out.println(f_hoy + " = " + f_hoy + "? " + Funciones.sonFechasIguales(Funciones.traerFecha(f_hoy), Funciones.traerFecha(f_hoy)));
+		System.out.println("\n");
+	}
+	
+	public static void main(String[] args) {
+		testBisiesto();
+		testTraerAnio();
+		testTraerMes();
+		testTraerDia();
+		testEsFechaValida();
+		testTraerFecha();
+		testTraerFechaCorta();
+		testEsDiaHabil();
+		testTraerDiaDeLaSemana();
+		testCompara();
+	}
+}
