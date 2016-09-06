a0422567c802d7dc7823ed4eb4b983af82712fb4
Agustin Padron <agustinpadron94@gmail.com>
2016-09-06 17:51:59 -0300

    Mejora de legibilidad

--- src/modelo/Rodado.java
+++ src/modelo/Rodado.java
@@ -4,8 +4,8 @@ public class Rodado {
 	private int idRodado;
 	private String dominio, marca;
 	private int modelo;
+	
 	public Rodado(int idRodado, String dominio, String marca, int modelo) {
-		super();
 		this.idRodado = idRodado;
 		this.dominio = dominio;
 		this.marca = marca;
@@ -14,27 +14,35 @@ public class Rodado {
 	public int getIdRodado() {
 		return idRodado;
 	}
+	
 	public void setIdRodado(int idRodado) {
 		this.idRodado = idRodado;
 	}
+	
 	public String getDominio() {
 		return dominio;
 	}
+	
 	public void setDominio(String dominio) {
 		this.dominio = dominio;
 	}
+	
 	public String getMarca() {
 		return marca;
 	}
+	
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+	
 	public int getModelo() {
 		return modelo;
 	}
+	
 	public void setModelo(int modelo) {
 		this.modelo = modelo;
 	}
+	
 	@Override
 	public String toString() {
 		return "[idRodado=" + idRodado + ", dominio=" + dominio + ", marca=" + marca + ", modelo=" + modelo	+ "]";
